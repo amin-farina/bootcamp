5ecfcac712aa4238068f16eddc4b5581d4e50848
Sebastian Amin Fariña <amiin74@gmail.com>
2022-11-28 20:34:56 -0300

    First commit with 30 exercise

--- node/practica.js
+++ node/practica.js
@@ -24,11 +24,31 @@ const indefinido = undefined;
 const nulo = null;
 const simbolos = Symbol();
 
-console.log("Tipo variable: ", typeof variable);
-console.log("Tipo edad: ", typeof edad);
-console.log("Tipo funcion: ", typeof mayorQue30);
-console.log("Tipo casa: ", typeof casa);
-console.log("Tipo indefinido: ", typeof indefinido);
-console.log("Tipo nulo: ", typeof nulo);
-console.log("Tipo simbolos: ", typeof simbolos);
-console.log("Color de casa: ", casa.color);
+// console.log("Tipo variable: ", typeof variable);
+// console.log("Tipo edad: ", typeof edad);
+// console.log("Tipo funcion: ", typeof mayorQue30);
+// console.log("Tipo casa: ", typeof casa);
+// console.log("Tipo indefinido: ", typeof indefinido);
+// console.log("Tipo nulo: ", typeof nulo);
+// console.log("Tipo simbolos: ", typeof simbolos);
+// console.log("Color de casa: ", casa.color);
+
+
+
+// Comparaciones
+
+// No estricta
+// console.log(2 == "2")
+
+//  Estricta
+// console.log(2 === "2")
+// console.log(2 !== "2")
+
+
+console.log("Saludo: ", typeof variable.toString())
+
+
+// Ternarios
+
+let ternario = 2 > 2 ? "Es mayor" : 2 < 2 ? "es menor" :  "es igual"
+console.log(ternario)
