2939cb1d8678c9066f9dc357cc0111c0cab3c61d
Sebastian Amin Fariña <amiin74@gmail.com>
2022-11-29 00:05:44 -0300

    Change file directory, and finish 40 exercise

--- 1-tarea.js
+++ 1-tarea.js
@@ -161,14 +161,61 @@
         myStr += " This is the second sentence.";
 
 // Ejercicio 30
-        //
+        // Constructing Strings with Variables
+        const myName = "Amin";
+        const myStr = "My name is " + myName + "and I am well!";  
+
 // Ejercicio 31
+        // Appending varaibles to strings
+        const someAdjective = "good";
+        let myStr = "Learning to code is ";
+        myStr += someAdjective;
+
 // Ejercicio 32
+        //Find the Length of a String
+        let lastNameLength = 0;
+        const lastName = "Lovelace";
+        lastNameLength = lastName.length;
+
 // Ejercicio 33
+        // Use Bracket Notation to Find the First Character in a String
+        let firstLetterOfLastName = "";
+        const lastName = "Lovelace";
+        firstLetterOfLastName = lastName[0];
+
 // Ejercicio 34
+        // Understand String Immutability
+        let myStr = "Jello World";
+        myStr = "Hello World"; // myStr[0] = "H" not is possible
+
 // Ejercicio 35
+        // Use Bracket Notation to Find the Nth Character in a String
+        const lastName = "Lovelace";
+        const thirdLetterOfLastName = lastName[2];
+
 // Ejercicio 36
+        // Use Bracket Notation to Find the Last Character in a String
+        const lastName = "Lovelace";
+        const lastLetterOfLastName = lastName[lastName.length - 1]; 
+
 // Ejercicio 37
+        // Use Bracket Notation to Find the Nth-to-Last Character in a String
+        const lastName = "Lovelace";
+        const secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
 // Ejercicio 38
+        // Word Blanks
+        const myNoun = "dog";
+        const myAdjective = "big";
+        const myVerb = "ran";
+        const myAdverb = "quickly";
+
+        const wordBlanks = myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb;
+
 // Ejercicio 39
+        //Store Multiple Values in one Variable using JavaScript Arrays
+        const myArray = ["String", 9];
+
 // Ejercicio 40
+        // Nest one Array within Another Array
+        const myArray = [["String1", 9],["String2", 8]];
