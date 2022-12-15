006cb59f5c24da5a2ed00ddefd7bc5f33562f715
Sebastian Amin Fariña <amiin74@gmail.com>
2022-12-15 16:29:09 -0300

    Practicando clase 4

--- node/server/index.js
+++ node/server/index.js
@@ -1,12 +1,62 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((request,response) => {
-    response.writeHead(200, "Exito", {"Content-Type" : "text/html" });
+    response.writeHead(200, "Success", {"Content-Type" : "application/pdf" });
 
-    console.log("request", request)
+    //EXAMPLE PDF 
+
+    // content-type : application/pdf
+
+    // response.writeHead(200, "Success", {"Content-Type" : "application/pdf" });
+
+    // fs.readFile('./Fariña-Sebastian-Amin-cv.pdf', (err, data) =>{
+    //     if (err){
+    //         console.log("No se pudo leer el archivo");
+    //         throw err;
+    //     }
+
+    //     response.end(data)
+    // })
+
+
+
+    // EXAMPLE
+    //
+    // content-type: application/json
+    //
+    // response.writeHead(200, "Success", {"Content-Type" : "application/json" });
+    //
+    // const json = {
+    //     status: 200,
+    //     data: [{
+    //         name: "Amin",
+    //         apellido: "Fariña"
+    //     }]
+    // }
+
+    // response.end(JSON.stringify(json))
+
+
+
+
+    //EXAMPLE WITH FILE .HTML
+    //
+    // Content-Type: text/html
+    // response.writeHead(200, "Success", {"Content-Type" : "text/html" });
+    // fs.readFile("./index.html", (err, data) => {
+    //     if(err){
+    //        fs.readFile("./error.html",(error,data2) =>{
+    //         if(error){
+    //             throw error;
+    //         }
+    //             response.end(data2);
+    //        })
+    //     }else{
+    //         response.end(data)
+    //     }
+    // });
     console.log("Servidor esta funcionando");
-
-    response.end("Respuesta finalizada")
 })
 
 server.listen(3000, "127.0.0.1", (err) => {
